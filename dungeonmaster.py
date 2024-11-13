ceb51c478c0783aa8c53fbda3c4c8aae7fd1b5a3
rflorys <robin.florys@hotmail.com>
2024-11-13 09:24:49 +0100

    Update dungeonmaster.py

--- dungeonmaster.py
+++ dungeonmaster.py
@@ -1,6 +1,5 @@
 import random
 
-
 def generer_niveau():
     liste = [random.choice([0, 1]) for i in range(20)]
     return liste
@@ -9,8 +8,8 @@ def lance_de():
     return random.randint(1, 6)
 
 
-salle = generer_niveau()
-print(salle)
+#salle = generer_niveau()
+#print(salle)
 
 #for i in range(3): 
 #    print(generer_niveau())
@@ -19,19 +18,39 @@ def dungeon(salle):
     vie = 1
     for o in salle:
         if o == 0: #Salle sans ennemi
-            print("La salle est vide")
+            #print("La salle est vide")
+            pass
         else: #Salle avec un ennemi 
             de = 0
             de = lance_de()
-            print(de)
+            #print(de)
             if de in [1, 2, 3]:
                 vie -= 1
-                print("Votre vie a atteint ",vie)
-                return False
+                #print("Votre vie a atteint ",vie)
             else:
-                print("Bravo (la traversée du niveau a été réussie)")
+                #print("Bravo (la traversée du niveau a été réussie)")
+                pass
+    if vie<=0:
+        #print("Votre vie à atteints ",vie,", vous êtes mort dans le dungeon")
+        return False
+    else:
+        #print("Bravo vous avez réussit la traverser du dungeon")
+        return True
+        
+#print(dungeon(salle))
 
-dungeon(salle)
+def main():
+    nombre_de_dungeon = 0
+    dungeon_passer = False
+    while not dungeon_passer:
+        nombre_de_dungeon = nombre_de_dungeon + 1
+        salle = 0
+        salle = generer_niveau()
+        print(salle)
+        dungeon_passer=dungeon(salle)
+    print("Jeu terminé ! ",nombre_de_dungeon, " niveaux traversés avant de réussir")
+    
+main()
 
 
                 
